da28c3574048201a937d495db6422824b2b2912b
Reed Hsin <hooy123456@gapp.nthu.edu.tw>
2024-01-23 23:49:38 +0800

    Define `RequestTypeSpy` for test

--- Tests/RHNetworkTests_EndToEndTests/RHNetworkTests_EndToEndTests.swift
+++ Tests/RHNetworkTests_EndToEndTests/RHNetworkTests_EndToEndTests.swift
@@ -9,3 +9,12 @@ import XCTest
 @testable import RHNetwork
 
 final class RHNetworkAPITests_EndToEndTests: XCTestCase {}
+private extension RHNetworkAPITests_EndToEndTests {
+    struct RequestTypeSpy: RequestType {
+        var headers: [String : String]? { nil }
+        var body: Data? { nil }
+        var baseURL: URL { .init(string: "https://pokeapi.co/api/v2")! }
+        var path: String { "pokemon-color/1" }
+        var method: HTTPMethod { .get }
+    }
+}
