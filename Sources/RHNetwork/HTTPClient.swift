0d6497b4333e5e08f4fdef1b1c9573f1c1e1de1b
Reed Hsin <hooy123456@gapp.nthu.edu.tw>
2024-01-23 23:15:18 +0800

    Implement `HTTPClient`

---
+++ Sources/RHNetwork/HTTPClient.swift
@@ -0,0 +1,22 @@
+//
+//  HTTPClient.swift
+//
+//
+//  Created by Chung Han Hsin on 2024/1/23.
+//
+
+import Foundation
+
+public enum HTTPClientError: Error {
+    case responseError
+    case cannotFindDataOrResponse
+}
+
+public enum HTTPClientResult {
+    case success(Data, HTTPURLResponse)
+    case failure(HTTPClientError)
+}
+
+public protocol HTTPClient {
+    func request(with request: RequestType, completion: @escaping (HTTPClientResult) -> Void)
+}
