d20061fdbc4cefac208615f64c0c0d4a25527f60
Reed Hsin <hooy123456@gapp.nthu.edu.tw>
2024-01-26 16:20:10 +0800

    Rename test name to `test_request_matchesBlackPokemonsData`

--- Tests/RHNetworkTests_EndToEndTests/RHNetworkTests_EndToEndTests.swift
+++ Tests/RHNetworkTests_EndToEndTests/RHNetworkTests_EndToEndTests.swift
@@ -9,29 +9,50 @@ import XCTest
 @testable import RHNetwork
 
 final class RHNetworkAPITests_EndToEndTests: XCTestCase {
-    func test_request_matchesPokemonPikachuData() {
-        if let receivedData = getPikachuData(),
-            let json = try? JSONSerialization.jsonObject(with: receivedData, options: []) as? [String: Any] {
+    func test_request_matchesBlackPokemonsData() {
+        if 
+            let receivedData = getBlackPokemonsData(),
+            let json = try? JSONSerialization.jsonObject(with: receivedData, options: []) as? [String: Any] 
+        {
             guard let pokemon_species = json["pokemon_species"] as? [[String: Any]] else {
                 XCTFail("Expected pokemon_species, but got no pokemon_species!")
                 return
             }
+            print(pokemon_species[0])
             XCTAssertEqual(pokemon_species.count, 51)
         }
     }
 }
 
 private extension RHNetworkAPITests_EndToEndTests {
-    struct RequestTypeSpy: RequestType {
+    struct BlackPokemonsRequest: RequestType {
+        var queryItems: [URLQueryItem] = []
         var headers: [String : String]? { nil }
         var body: Data? { nil }
         var baseURL: URL { .init(string: "https://pokeapi.co/api/v2")! }
-        var path: String { "pokemon-color/1" }
+        var path: String { "/pokemon-color/black" }
         var method: HTTPMethod { .get }
     }
     
-    func getPikachuData(file: StaticString=#file, line: UInt=#line) -> Data? {
-        let request = RequestTypeSpy()
+    func getBlackPokemonsData(file: StaticString=#file, line: UInt=#line) -> Data? {
+        let request = BlackPokemonsRequest()
+        // ephemeral: we don't need to cahce for session-related data
+        let client = URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
+        var receivedData: Data?
+        let exp = expectation(description: "wait for the response ...")
+        client.request(with: request) { result in
+            switch result {
+            case let .success(data, _):
+                receivedData = data
+            default:
+                XCTFail("Expected success, got \(result) instead", file: file, line: line)
+            }
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 30.0)
+        return receivedData
+    }
+    
         // ephemeral: we don't need to cahce for session-related data
         let client = URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
         var receivedData: Data?
